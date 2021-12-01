9299b5cdc963c610d2e69b5258b9dbf45f74d6d5
Vusalmb <53233126+Vusalmb@users.noreply.github.com>
2021-12-01 12:18:18 +0400

    test

---
+++ Task7/assets/JS/script.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+    $("#text-1").click(function(){
+        $("#text-item-1").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down1").hide();
+        $("#up1").show();
+        $("#text-1").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-2").click(function(){
+        $("#text-item-2").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down2").hide();
+        $("#up2").show();
+        $("#text-2").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-3").click(function(){
+        $("#text-item-3").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down3").hide();
+        $("#up3").show();
+        $("#text-3").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-4").click(function(){
+        $("#text-item-4").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down4").hide();
+        $("#up4").show();
+        $("#text-4").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-5").click(function(){
+        $("#text-item-5").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down5").hide();
+        $("#up5").show();
+        $("#text-5").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-6").click(function(){
+        $("#text-item-6").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down6").hide();
+        $("#up6").show();
+        $("#text-6").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-7").click(function(){
+        $("#text-item-7").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down7").hide();
+        $("#up7").show();
+        $("#text-7").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-8").click(function(){
+        $("#text-item-8").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down8").hide();
+        $("#up8").show();
+        $("#text-8").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
+
+$(document).ready(function(){
+    $("#text-9").click(function(){
+        $("#text-item-9").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+
+        $("#down9").hide();
+        $("#up9").show();
+        $("#text-9").show();
+        $("#menu-desc li").css("flex-wrap", "wrap");
+    });
+});
